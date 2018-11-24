33b67ceeda6eacd7ed75dcc29536b521f6eb1845
Michal Jakobczyk <mtjakobczyk@gmail.com>
2018-11-24 11:59:40 +0100

    OCI #10 - source code - update

--- oci-10/oci/create-ocir-policy.sh
+++ oci-10/oci/create-ocir-policy.sh
@@ -7,6 +7,10 @@ OCI_GROUP="oraas-developers"
 OCI_PROJECT_PREFIX="p-oraas"
 
 # Add a new policy statements to let group member manage OCIR repositories with a specific naming pattern
-OCI_POLICY_INSPECT="allow group $OCI_GROUP to inspect repos in tenancy where request.operation='ListDockerRepositories'"
 OCI_POLICY_MANAGE="allow group $OCI_GROUP to manage repos in tenancy where target.repo.name = /$OCI_PROJECT_PREFIX-*/"
-oci iam policy create -c $OCI_POLICY_COMPARTMENT_OCID --name $OCI_POLICY_NAME --description "$OCI_POLICY_DESC" --statements "[ \"$OCI_POLICY_INSPECT\", \"$OCI_POLICY_MANAGE\" ]"
+oci iam policy create -c $OCI_POLICY_COMPARTMENT_OCID --name $OCI_POLICY_NAME --description "$OCI_POLICY_DESC" --statements "[ \"$OCI_POLICY_MANAGE\" ]"
+
+# Alternatively you can additionally let user see a list of all images in OCIR (only names):
+# OCI_POLICY_INSPECT="allow group $OCI_GROUP to inspect repos in tenancy where request.operation='ListDockerRepositories'"
+# OCI_POLICY_MANAGE="allow group $OCI_GROUP to manage repos in tenancy where target.repo.name = /$OCI_PROJECT_PREFIX-*/"
+# oci iam policy create -c $OCI_POLICY_COMPARTMENT_OCID --name $OCI_POLICY_NAME --description "$OCI_POLICY_DESC" --statements "[ \"$OCI_POLICY_INSPECT\", \"$OCI_POLICY_MANAGE\" ]"
