92b6aa15878e702d675b814922abf41533ad6758
Michal Jakobczyk <mtjakobczyk@gmail.com>
2018-11-24 13:48:21 +0100

    OCI #10 -source code - update

--- oci-10/dockerize.sh
+++ oci-10/dockerize.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # Customize here - OCI details
+OCI_PROJECT_CODE={put-here-your-project-code}
+OCI_TENANCY={put-here-your-tenancy-name}
 OCIR_REGION=fra
-OCI_TENANCY=<put-here-your-tenancy-name>
-OCI_USER=oraas-dev-1
-OCI_PROJECT=p-oraas
+OCI_USER="$OCI_PROJECT_CODE-dev-1"
+
 # Customize here - Your image details
 IMAGE_NAME=simple-api
-IMAGE_TAG=1.1
+IMAGE_TAG=1.0
 
 # Push the image into Oracle Container Image Registry
-git clone https://github.com/mtjakobczyk/blog-code.git
-cd blog-code/oci-10/app
 docker build -t $IMAGE_NAME:$IMAGE_TAG .
-docker tag $IMAGE_NAME:$IMAGE_TAG $OCIR_REGION.ocir.io/$OCI_TENANCY/$OCI_PROJECT-$IMAGE_NAME:$IMAGE_TAG
+docker tag $IMAGE_NAME:$IMAGE_TAG $OCIR_REGION.ocir.io/$OCI_TENANCY/p-$OCI_PROJECT-$IMAGE_NAME:$IMAGE_TAG
 docker login -u $OCI_TENANCY/$OCI_USER $OCIR_REGION.ocir.io
 ### You will be prompted for the auth token here
-docker push $OCIR_REGION.ocir.io/$OCI_TENANCY/$OCI_PROJECT-$IMAGE_NAME:$IMAGE_TAG
+docker push $OCIR_REGION.ocir.io/$OCI_TENANCY/p-$OCI_PROJECT-$IMAGE_NAME:$IMAGE_TAG
--- oci-10/oci/create-ocir-policy.sh
+++ oci-10/oci/create-ocir-policy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Customize here
-OCI_POLICY_COMPARTMENT_OCID=<put-here-your-compartment-OCID-for-policy>
-OCI_POLICY_NAME="p-oraas-ocir-policy"
-OCI_POLICY_DESC="Operation Research as a Service project OCIR policy"
-OCI_GROUP="oraas-developers"
-OCI_PROJECT_PREFIX="p-oraas"
+OCI_POLICY_COMPARTMENT_OCID={put-here-your-compartment-OCID-for-policy}
+OCI_PROJECT_CODE={put-here-your-project-code}
+OCI_POLICY_NAME="p-$OCI_PROJECT_CODE-ocir-policy"
+OCI_POLICY_DESC="$OCI_PROJECT_CODE project OCIR policy"
+OCI_GROUP="$OCI_PROJECT_CODE-developers"
+OCI_PROJECT_PREFIX="p-$OCI_PROJECT_CODE"
 
 # Add a new policy statements to let group member manage OCIR repositories with a specific naming pattern
 OCI_POLICY_MANAGE="allow group $OCI_GROUP to manage repos in tenancy where target.repo.name = /$OCI_PROJECT_PREFIX-*/"
